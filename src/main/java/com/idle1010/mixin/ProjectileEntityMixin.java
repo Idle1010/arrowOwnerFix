feaf2833f358bd9b3dc622feb6438e8adfc03cfc
Idle1010 <94646513+Idle1010@users.noreply.github.com>
2025-09-07 18:15:52 +0200

    Fixed Owner resetting on Restart

--- src/main/java/com/idle1010/mixin/ProjectileEntityMixin.java
+++ src/main/java/com/idle1010/mixin/ProjectileEntityMixin.java
@@ -1,26 +1,43 @@
 package com.idle1010.mixin;
 
-import net.minecraft.entity.ProjectileDeflection;
-import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LazyEntityReference;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.UUID;
+
 @Mixin(ProjectileEntity.class)
 public abstract class ProjectileEntityMixin {
 
-	@Redirect(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileEntity;deflect(Lnet/minecraft/entity/ProjectileDeflection;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity;Z)Z"))
-	private boolean redirectDeflect(ProjectileEntity instance, ProjectileDeflection deflection, @Nullable net.minecraft.entity.Entity deflector, @Nullable net.minecraft.entity.Entity owner, boolean fromAttack) {
-		if (instance instanceof PersistentProjectileEntity) {
-			deflection.deflect(instance, deflector, instance.random);
-			if (!instance.getWorld().isClient) {
-				instance.onDeflected(deflector, fromAttack);
-			}
+	@Redirect(method = "deflect", at=@At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileEntity;setOwner(Lnet/minecraft/entity/Entity;)V"))
+	private void redirectSetOwner(ProjectileEntity instance, @Nullable Entity owner) {}
 
-			return true;
+	@Redirect(method="readCustomData", at=@At(value = "INVOKE", target = "Lnet/minecraft/entity/LazyEntityReference;fromData(Lnet/minecraft/storage/ReadView;Ljava/lang/String;)Lnet/minecraft/entity/LazyEntityReference;"))
+	private LazyEntityReference<?> redirectFromData(ReadView view, String key) {
+		String strUUID = view.getString(key, "");
+		if (strUUID.isEmpty()) {
+			return null;
 		}
-		return instance.deflect(deflection, deflector, owner, fromAttack);
+        return new LazyEntityReference<>(UUID.fromString(strUUID));
+	}
+
+	@Redirect(method="writeCustomData", at=@At(value = "INVOKE", target = "Lnet/minecraft/entity/LazyEntityReference;writeData(Lnet/minecraft/entity/LazyEntityReference;Lnet/minecraft/storage/WriteView;Ljava/lang/String;)V"))
+	private void redirectWriteData(LazyEntityReference<Entity> reference, WriteView view, String key) {
+		if (reference == null) {
+			view.putString(key, "");
+			return;
+		}
+		UUID uuid = reference.getUuid();
+		if (uuid != null) {
+			view.putString(key, uuid.toString());
+		} else {
+			view.putString(key, "");
+		}
 	}
 }
